2b13053806a421a235f5b7587615753b81576b1e
daddandrea <andrea.daddabbo@gmail.com>
2026-03-20 01:07:55 +0100

    Created docker configuration

--- pricewatch/settings.py
+++ pricewatch/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 CELERY_BEAT_SCHEDULE = {
     "fetch-prices-every-hour": {
